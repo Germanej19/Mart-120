057477959908cafda97b156fc494426153ee1f69
Germanej19 <ravvy19@gmail.com>
2023-04-25 14:44:57 -0600

    Week 12 Homework
    I forgot that because of spring break we are a week behind the calendar in terms of school. My other week 12 submission is my week 11

---
+++ week 13/js/sketch.js
@@ -0,0 +1,118 @@
+// x and y for my character
+var characterX = 100;
+var characterY = 100;
+// define the key codes for each letter
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
+
+// x and y for a shape
+var shapeX = 30;
+var shapeY = 50;
+var shapeXSpeed;
+var shapeYSpeed;
+
+// create a shape when the mouse is clicked
+var mouseShapeX;
+var mouseShapeY;
+function setup()
+{
+    createCanvas(500, 600);
+    // get a random speed when it first starts
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createCharacter(200, 350);
+}
+
+function draw()
+{
+    background(120, 45, 78);
+    stroke(0);
+    fill(0);
+
+    // call createBorders function
+    createBorders(10);
+
+    // exit message
+    textSize(16);
+    text("EXIT", width - 50, height - 50);
+
+    //createCharacter(200,350);
+    drawCharacter();
+    characterMovement();
+
+
+    // potential enemy
+    fill(13, 145, 14);
+    // draw the shape
+    circle(shapeX, shapeY, 10);
+
+    // get a random speed when it first starts
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+    // move the shape
+    shapeX += shapeXSpeed;
+    shapeY += shapeYSpeed;
+    // check to see if the shape has gone out of bounds
+    if (shapeX > width) {
+        shapeX = 0;
+    }
+    if (shapeX < 0) {
+        shapeX = width;
+    }
+    if (shapeY > height) {
+        shapeY = 0;
+    }
+    if (shapeY < 0) {
+        shapeY = height;
+    }
+
+    // check to see if the character has left the exit
+    if (characterX > width && characterY > width - 50) {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("You Win!", width / 2 - 50, height / 2 - 50);
+    }
+
+    // create the shape based on the mouse click
+    fill(120, 130, 140);
+    circle(mouseShapeX, mouseShapeY, 25);
+}
+
+function characterMovement()
+{
+    // handle the keys
+    if (keyIsDown(w)) {
+        characterY -= 10;
+    }
+    if (keyIsDown(s)) {
+        characterY += 10;
+    }
+    if (keyIsDown(a)) {
+        characterX -= 10;
+        console.log("movement: " + characterX);
+    }
+    if (keyIsDown(d)) {
+        characterX += 10;
+    }
+}
+
+function createCharacter(x, y)
+{
+    characterX = x;
+    characterY = y;
+    console.log(characterX);
+    //character
+
+    // circle(characterX,characterY,25);
+}
+
+function drawCharacter()
+{
+    fill(23, 40, 123);
+    circle(characterX, characterY, 25);
+}
+
